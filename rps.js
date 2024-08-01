fff10b6d3502a9ec0c083f97ab577bff12587fb0
laurenskr <laurens.krom@gmail.com>
2024-08-01 19:02:04 +0200

    added function getComputerChoice

--- rps.js
+++ rps.js
@@ -1,1 +1,21 @@
-console.log("Hello there!")
+console.log("Hello there!");
+
+function getComputerChoice() {
+    let strChoice;
+    let intChoice = Math.floor(Math.random()*3);
+    switch (intChoice) {
+        case 0:
+            strChoice = "rock";
+            break;
+        case 1:
+            strChoice = "paper";
+            break;
+        case 2:
+            strChoice = "scissors"
+            break 
+    }
+    return strChoice
+
+}
+console.log(getComputerChoice())
+
